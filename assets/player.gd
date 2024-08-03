0ff29141267b5e6acb49a9456a08130bad068593
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-03 05:38:50 -0300

    a

--- assets/player.gd
+++ assets/player.gd
@@ -16,6 +16,18 @@ func move(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+
+enum wepon_enum {
+	whater_can = 0,
+	pistol = 1,
+	smg = 2,
+	shotgum = 3
+}
+
+@export var wepon_selected : wepon_enum
+
+
 
 func look_around(delta):
 	var mouse_pos : Vector2 = $Camera3D.get_viewport().get_mouse_position()
@@ -29,7 +41,25 @@ func look_around(delta):
 	$player_model.look_at($player_model.global_position + target_pos,Vector3.UP)
 	$player_model.rotation.y = -$player_model.rotation.y
 
+@export var gunModelPath : NodePath
+
+@export var whater_can : Mesh
+@export var pistol : Mesh
+@export var smg : Mesh
+@export var shotgum : Mesh
+
+func make_gun_stuf():
+	if wepon_selected == 0:
+		get_node(gunModelPath).mesh = whater_can
+	elif wepon_selected == 1:
+		get_node(gunModelPath).mesh = pistol
+	elif wepon_selected == 2:
+		get_node(gunModelPath).mesh = smg
+	elif wepon_selected == 3:
+		get_node(gunModelPath).mesh = shotgum
+
 func _physics_process(delta):
 	move(delta)
 	look_around(delta)
+	make_gun_stuf()
 	
