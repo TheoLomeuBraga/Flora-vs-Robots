274155b37f7ec28ceb1c588288660b5dd383f505
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-04 13:37:17 -0300

    a

--- wave_master.gd
+++ wave_master.gd
@@ -2,11 +2,36 @@ extends Node3D
 
 @export var wave_intensity : int = 0
 
+@export var enemy_asset : PackedScene
+
+var camera : Camera3D
+
 func _ready():
-	pass # Replace with function body.
+	camera = get_tree().get_root().get_node("GameSceane/Player/Camera3D")
 
 func stop_wave():
 	wave_intensity = 0
+	
+func spawn_enemy():
+	var spawns = $spawns.get_children()
+	var tries = spawns.size()
+	
+	while tries > 0:
+		var index = randi() % spawns.size()
+		var spawn = spawns[index]
+		
+		if !camera.is_position_in_frustum(spawn.global_position):
+			var enemy : Node3D = enemy_asset.instantiate()
+			enemy.global_position = spawn.global_position
+			get_tree().get_root().add_child(enemy)
+			break
+		
+		tries -= 1
 
+var time_to_next_spawn : float = 1.0
 func _process(delta):
-	pass
+	if time_to_next_spawn <= 0:
+		time_to_next_spawn = 5.0 / float(wave_intensity)
+		spawn_enemy()
+	
+	time_to_next_spawn -= delta
